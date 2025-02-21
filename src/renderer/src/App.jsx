a420f6de8a0fd46f32654f97542f34e0b69542fb
virtualolsonNo1 <virtualolson.no1@gmail.com>
2025-02-20 23:02:14 -0600

    can choose random opening

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -2,6 +2,16 @@ import { useState } from 'react'
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js';
 
+
+
+const openings = { 
+     random: '',
+     italian: 'r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3',
+     sicilian: 'rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2',
+     french: 'rnbqkbnr/ppp2ppp/4p3/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq - 0 3',
+     caro: 'rnbqkbnr/pp2pppp/2p5/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq - 0 3'
+};
+
 function App() {
   // Initialize the game state with a new Chess instance
   const [game, setGame] = useState(new Chess());
@@ -28,6 +38,17 @@ function App() {
       return false;
     }
   }
+  
+  function loadRandomPosition(opening) {
+  }
+  
+  function displayOpening(opening) {
+    // Update the game state
+    console.log(opening)
+    console.log(openings[opening])
+
+    setGame(new Chess(openings[opening]));
+  }
 
   return (
   <div className="container mx-auto p-4">
@@ -42,7 +63,12 @@ function App() {
         <div className="bg-gray-100 p-4 rounded">
           <h2>Position Information</h2>
           <p>{feedback}</p>
-          <button /*onClick={loadRandomPosition}*/>Next Position</button>
+          <select onChange={(e) => displayOpening(e.target.value)}>
+            <option value="random">Random</option>
+            <option value="italian">Italian Game</option>
+            <option value="sicilian">Sicilian Defense</option>
+          </select>
+          <button onClick={loadRandomPosition}>Next Position</button>
         </div>
       </div>
       
